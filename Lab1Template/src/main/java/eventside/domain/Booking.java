bd4056d76bf19411cd4653551d22eaa82bfacb12
Unterkofler <82438419+Unterkofler@users.noreply.github.com>
2022-03-25 10:18:45 +0100

    Add files via upload

---
+++ Lab1Template/src/main/java/eventside/domain/Booking.java
@@ -0,0 +1,14 @@
+package eventside.domain;
+
+import java.time.LocalDate;
+
+public class Booking {
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int roomNumber;
+    private Customor personalData;
+    private int reservationNumber;
+
+    public Booking(){
+    }
+}
---
+++ Lab1Template/src/main/java/eventside/domain/Room.java
@@ -0,0 +1,10 @@
+package eventside.domain;
+
+public class Room {
+    private int roomNumber;
+    private boolean isFree = true;
+
+    public Room (int roomNumber){
+        this.roomNumber = roomNumber;
+    }
+}
