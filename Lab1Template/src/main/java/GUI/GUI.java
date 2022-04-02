4cd0ac79d227197a12c3eb6c488d77d49603749a
ShortyIT <tobias.kurz@students.fhv.at>
2022-04-02 16:48:57 +0200

    startet with UI

--- Lab1Template/src/main/java/GUI/GUI.java
+++ Lab1Template/src/main/java/GUI/GUI.java
@@ -2,9 +2,7 @@ package GUI;
 
 import eventside.domain.ValueObjects.BookingId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import writeside.application.interfaces.HotelService;
 import writeside.application.interfaces.RepositoryWrite;
@@ -19,7 +17,7 @@ import java.util.UUID;
 
 @Component
 @Service
-public final class GUI implements ActionListener{
+public final class GUI implements ActionListener {
     @Autowired
     private RepositoryWrite storageWrite = new RepositoryWriteImpl();
 
@@ -27,27 +25,22 @@ public final class GUI implements ActionListener{
     private HotelService hotelService;
 
     private JLabel labelFirstName, labelLastName, labelStartDate, labelEndDate;
-    private JTextField textFieldFirstName, textFieldLastName, textFieldStartDate, textFieldEndDate;
+    private JTextField textFieldFirstName, textFieldLastName, textFieldStartDate, textFieldEndDate, textFieldCapacity;
     private JButton buttonConfirm;
     private JPanel panel;
     private JFrame frame;
-    private static String firstName, lastName, startDate, endDate;
+   // private static String firstName, lastName, startDate, endDate, capacity;
 
-    @Override
+   /* @Override
     public void actionPerformed(ActionEvent e) {
         String firstName = textFieldFirstName.getText();
         String lastName = textFieldLastName.getText();
         String startDate = textFieldStartDate.getText();
         String endDate = textFieldEndDate.getText();
+        String capacity = textFieldCapacity.getText();
+    } */
 
-      //  storageWrite.add(new Customer(firstName, lastName));
-        JOptionPane.showMessageDialog(null, "firstName: " + firstName + '\n' +
-                                                                    "lastName: " + lastName + '\n' +
-                                                                    "startDate: " + startDate + '\n' +
-                                                                    "endDate: " + endDate);
-    }
-
-    public void start(){
+    public void start() {
 
         buttonConfirm = new JButton("Confirm");
 
@@ -56,27 +49,29 @@ public final class GUI implements ActionListener{
         panel.setLayout(null);
 
         labelFirstName = new JLabel("First Name");
-        labelFirstName.setBounds(100, 10,70,20);
+        labelFirstName.setBounds(100, 10, 70, 20);
         panel.add(labelFirstName);
 
         labelLastName = new JLabel("Last Name");
-        labelLastName.setBounds(100,60,70,20);
+        labelLastName.setBounds(100, 60, 70, 20);
         panel.add(labelLastName);
 
-        labelStartDate = new JLabel("Start Date");
-        labelStartDate.setBounds(100, 110, 70,20);
+        labelStartDate = new JLabel("Start Date - Format: 2022-01-01");
+        labelStartDate.setBounds(100, 110, 70, 20);
         panel.add(labelStartDate);
 
-        labelEndDate = new JLabel("End Date");
-        labelEndDate.setBounds(100, 160, 70,20);
+        labelEndDate = new JLabel("End Date - Format: 2022-01-01");
+        labelEndDate.setBounds(100, 160, 70, 20);
+        panel.add(labelEndDate);
+
+        labelEndDate = new JLabel("Capacity");
+        labelEndDate.setBounds(100, 210, 70, 20);
         panel.add(labelEndDate);
 
 
-
-
-        buttonConfirm = new JButton("Login");
+        buttonConfirm = new JButton("Book Room");
         buttonConfirm.setFont(new Font("Dialog", Font.PLAIN, 12));
-        buttonConfirm.setBounds(100, 230, 100, 28);
+        buttonConfirm.setBounds(100, 300, 100, 28);
         buttonConfirm.setForeground(Color.WHITE);
         buttonConfirm.setBackground(Color.BLACK);
         buttonConfirm.addActionListener(this);
@@ -86,30 +81,32 @@ public final class GUI implements ActionListener{
         textFieldLastName = new JTextField();
         textFieldEndDate = new JTextField();
         textFieldStartDate = new JTextField();
+        textFieldCapacity = new JTextField();
 
-        textFieldFirstName.setBounds(100,30,200,28);
-        textFieldLastName.setBounds(100,80,200,28);
-        textFieldStartDate.setBounds(100,130,200,28);
-        textFieldEndDate.setBounds(100,180,200,28);
+        textFieldFirstName.setBounds(100, 30, 200, 28);
+        textFieldLastName.setBounds(100, 80, 200, 28);
+        textFieldStartDate.setBounds(100, 130, 200, 28);
+        textFieldEndDate.setBounds(100, 180, 200, 28);
+        textFieldCapacity.setBounds(100, 230, 200, 28);
 
         panel.add(textFieldFirstName);
         panel.add(textFieldLastName);
         panel.add(textFieldEndDate);
         panel.add(textFieldStartDate);
+        panel.add(textFieldCapacity);
 
-        //hier kommt die Exception Achim!!!
         frame = new JFrame("BookRoom");
         //frame.setTitle("BookRoom");
         frame.setLocation(new Point(500, 300));
         frame.add(panel);
-        frame.setSize(600,600);
+        frame.setSize(600, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
         buttonConfirm.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
-                    hotelService.createBooking("Tobias","Kurz",new BookingId(UUID.randomUUID()), LocalDate.now(),LocalDate.now(),2);
+                    hotelService.createBooking(getFirstName(), getLastName(), new BookingId(UUID.randomUUID()), getStartDate(), getEndDate(), getCapacity());
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
@@ -117,19 +114,29 @@ public final class GUI implements ActionListener{
         });
     }
 
+
     public String getFirstName() {
-        return firstName;
+        return textFieldFirstName.getText();
     }
 
     public String getLastName() {
-        return lastName;
+        return textFieldLastName.getText();
     }
 
-    public String getStartDate() {
-        return startDate;
+    public LocalDate getStartDate() {
+        return LocalDate.parse(textFieldStartDate.getText());
     }
 
-    public String getEndDate() {
-        return endDate;
+    public LocalDate getEndDate() {
+        return LocalDate.parse(textFieldEndDate.getText());
+    }
+
+    public int getCapacity() {
+        return Integer.parseInt(textFieldCapacity.getText());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
